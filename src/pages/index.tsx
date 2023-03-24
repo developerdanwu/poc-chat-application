4a311531b4dfa0a4446d4bbd0443ee7ceb0a350e
Dan Wu <d.wu@sleekflow.io>
2023-03-25 00:47:51 +0800

    test: openai and chatgpt functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,18 +4,15 @@ import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { api } from "@/utils/api";
-import {invoke} from "@tauri-apps/api/tauri";
-import {useEffect} from "react";
-
-const isClient = typeof window !== 'undefined'
+import { invoke } from "@tauri-apps/api/tauri";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-  useEffect(() => {
-    invoke('greet', { name: 'World' })
-        .then(console.log)
-        .catch(console.error)
-  }, []);
+  const [text, setText] = useState("");
+  const [prevId, setPrevId] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
+  const sendApiPrompt = api.chatGpt.sendAiPrompt.useMutation();
+  const completionApi = api.chatGpt.createCompletion.useMutation();
   return (
     <>
       <Head>
@@ -24,6 +21,28 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <button
+          onClick={() => {
+            completionApi.mutate(
+              {
+                model: "text-davinci-003",
+                prompt: text,
+                ...(prevId && { parentMessageId: prevId }),
+              },
+              {
+                onSuccess: (data) => {
+                  if (data) {
+                    setMessages((prev) => [...prev, data]);
+                  }
+                },
+              }
+            );
+          }}
+        >
+          submit
+        </button>
+
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
@@ -53,10 +72,11 @@ const Home: NextPage = () => {
             </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p>
-            <AuthShowcase />
+            <div className="text-2xl text-white">
+              {messages.map((m) => {
+                return <p key={m}>{m}</p>;
+              })}
+            </div>
           </div>
         </div>
       </main>
@@ -65,27 +85,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,13 +1,12 @@
 import { createTRPCRouter } from "@/server/api/trpc";
-import { exampleRouter } from "@/server/api/routers/example";
-
+import { chatGpt } from "@/server/api/routers/chatGpt";
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  chatGpt,
 });
 
 // export type definition of API
