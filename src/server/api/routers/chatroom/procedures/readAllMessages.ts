0ca435b371b05a261c53573eeaead440ddfa16a6
Dan Wu <developerdanwu@gmail.com>
2023-10-17 21:10:33 +0800

    redo prisma schema

---
+++ src/server/api/routers/chatroom/procedures/readAllMessages.ts
@@ -0,0 +1,36 @@
+import { protectedProcedure } from '@/server/api/trpc';
+import { z } from 'zod';
+import { MessageStatus } from '@prisma-generated/generated/types';
+import { dbConfig, withAuthors } from '@/server/api/routers/helpers';
+
+const readAllMessages = protectedProcedure
+  .input(
+    z.object({
+      chatroomId: z.string().min(1),
+    })
+  )
+  .mutation(async ({ ctx, input }) => {
+    ctx.db
+      .updateTable('message')
+      .set((eb) => ({
+        status: MessageStatus.READ,
+      }))
+      .where((eb) =>
+        eb.and([
+          eb('chatroom_id', '=', input.chatroomId),
+          eb('status', '=', MessageStatus.DELIVERED),
+        ])
+      )
+      .execute();
+
+    const chatroom = await ctx.db
+      .selectFrom(`chatroom as ${dbConfig.tableAlias.chatroom}`)
+      .select((eb) => [...dbConfig.selectFields.chatroom, withAuthors(eb)])
+      .where((eb) =>
+        eb(`${dbConfig.tableAlias.chatroom}.id`, '=', input.chatroomId)
+      )
+      .groupBy(`${dbConfig.tableAlias.chatroom}.id`)
+      .executeTakeFirstOrThrow();
+  });
+
+export default readAllMessages;
