59e6ab016c80926008b06d710741f50f5b22db57
Dan Wu <d.wu@sleekflow.io>
2023-11-18 17:14:32 +0800

    add picture to readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a small POC for optimising chat applications through virtualisation techniques, effective state management
 and implementing rich text editor modelled after Slack. Real time events is handled by Ably and LiveBlocks while
 authentication is handled by Clerk third party services.
+
+<img src="./public/static/app-demo.png"></img>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 // @ts-ignore
-const extendedTheme = require('@/lib/extendedTheme.cjs')
+const extendedTheme = require('./src/lib/extendedTheme.cjs')
 module.exports = {
-    darkMode: ["class"],
-    content: [
-        './pages/**/*.{ts,tsx}',
-        './components/**/*.{ts,tsx}',
-        './app/**/*.{ts,tsx}',
-        './src/**/*.{ts,tsx}',
-    ],
-    theme: {
-        container: {
-            center: true,
-            padding: "2rem",
-            screens: {
-                "2xl": "1400px",
-            },
-        },
-        extend: {
-            ...extendedTheme
-        },
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
-    plugins: [require("tailwindcss-animate")],
+    extend: {
+      ...extendedTheme
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
 }
