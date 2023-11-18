16c878e815105674fb7ce2443e0a38693135eb77
Dan Wu <d.wu@sleekflow.io>
2023-11-18 16:11:15 +0800

    remove slack api stuff

--- src/app/api/slack/events/_events_handlers/_challenge.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export function challenge(body: any) {
-  console.log('req body challenge is:', body.challenge);
-
-  return NextResponse.json({
-    challenge: body.challenge,
-  });
-}
