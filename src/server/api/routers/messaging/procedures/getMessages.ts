b63d1a1e916662896a2226e5dea2ceaf57212a9f
Dan Wu <d.wu@sleekflow.io>
2024-08-12 11:58:18 +0800

    reformatting

--- src/server/api/routers/messaging/procedures/getMessages.ts
+++ src/server/api/routers/messaging/procedures/getMessages.ts
@@ -1,7 +1,7 @@
-import {protectedProcedure} from '@/server/api/trpc';
-import {z} from 'zod';
-import {MESSAGES_PER_PAGE} from '@/pages/[chatroomId]/_components/main/main-content/ChatWindow/constants';
-import {dbConfig} from '@/server/api/routers/helpers'; // implement a before and latest
+import {protectedProcedure} from "@/server/api/trpc";
+import {z} from "zod";
+import {MESSAGES_PER_PAGE} from "@/pages/[chatroomId]/_components/main/main-content/ChatWindow/constants";
+import {dbConfig} from "@/server/api/routers/helpers"; // implement a before and latest
 
 // implement a before and latest
 
@@ -9,12 +9,13 @@ const getMessages = protectedProcedure
   .input(
     z.object({
       chatroomId: z.string().min(1),
-      orderBy: z.enum(['asc', 'desc']).optional(),
+      orderBy: z.enum(["asc", "desc"]).optional(),
       cursor: z.date().nullish(),
       skip: z.number().optional(),
       take: z.number().optional(),
     })
   )
+
   .query(async ({ ctx, input }) => {
     const limit = input.take || MESSAGES_PER_PAGE;
     try {
@@ -25,15 +26,15 @@ const getMessages = protectedProcedure
             .select([...dbConfig.selectFields.message])
             .where((eb) => {
               return eb.and([
-                eb('chatroom_id', '=', input.chatroomId),
+                eb("chatroom_id", "=", input.chatroomId),
                 ...(input.cursor !== null && input.cursor !== undefined
-                  ? [eb('created_at', '<', input.cursor)]
+                  ? [eb("created_at", "<", input.cursor)]
                   : []),
               ]);
             })
             .limit(limit)
-            .orderBy('created_at', input.orderBy || 'desc')
-            .as('message');
+            .orderBy("created_at", input.orderBy || "desc")
+            .as("message");
         })
         .selectAll()
         .execute();
