601392c26adfca64a93694ec78d943266f6dbe11
Dan Wu <d.wu@sleekflow.io>
2024-08-12 11:59:28 +0800

    reformat

--- src/server/api/routers/messaging/messaging.ts
+++ src/server/api/routers/messaging/messaging.ts
@@ -1,9 +1,9 @@
-import { createTRPCRouter } from '@/server/api/trpc';
-import getMessages from '@/server/api/routers/messaging/procedures/getMessages';
-import sendMessage from '@/server/api/routers/messaging/procedures/sendMessage';
-import getMessagesCount from '@/server/api/routers/messaging/procedures/getTotalMessages';
-import readAllMessages from '@/server/api/routers/messaging/procedures/readAllMessages';
-import unreadMessages from '@/server/api/routers/messaging/procedures/unreadMessages';
+import { createTRPCRouter } from "@/server/api/trpc";
+import getMessages from "@/server/api/routers/messaging/procedures/getMessages";
+import sendMessage from "@/server/api/routers/messaging/procedures/sendMessage";
+import getMessagesCount from "@/server/api/routers/messaging/procedures/getTotalMessages";
+import readAllMessages from "@/server/api/routers/messaging/procedures/readAllMessages";
+import unreadMessages from "@/server/api/routers/messaging/procedures/unreadMessages";
 
 export const messaging = createTRPCRouter({
   getMessages,
